fd7d7feb237614a7987694fbc784aa7c928799c9
Faroukii <122316189+Faroukii@users.noreply.github.com>
2023-02-15 17:34:28 -0500

    fahrenheit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double fahrenheit = 0;
+        String trash = "";
+        System.out.println("Please enter temperature in Fahrenheit: ");
+
+        if (in.hasNextDouble())
+        {
+            fahrenheit = in.nextDouble();
+            double celsius = (fahrenheit * 1.8)+32;
+            System.out.println("The temperature in celsius is: " + celsius);
+        }
+        else
+        {
+        trash = in.nextLine();
+            System.out.println("Please enter correct value: " + trash);
+        }
     }
 }
