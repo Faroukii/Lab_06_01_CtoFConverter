6016ad29d9810c1738969849cadff3cd5fbfab6b
Faroukii <122316189+Faroukii@users.noreply.github.com>
2023-02-22 22:24:53 -0500

    fahrenheit

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,20 @@ public class Main {
         double fahrenheit = 0;
         String trash = "";
         System.out.println("Please enter temperature in Fahrenheit: ");
+        //Asking for Fahrenheit
 
         if (in.hasNextDouble())
         {
             fahrenheit = in.nextDouble();
             double celsius = (fahrenheit * 1.8)+32;
             System.out.println("The temperature in celsius is: " + celsius);
+            //Formula for the conversion from Fahrenheit to Celsius
         }
         else
         {
         trash = in.nextLine();
             System.out.println("Please enter correct value: " + trash);
+            //Trash if input is anything other than double
         }
     }
 }
